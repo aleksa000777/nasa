d58d506948643907c1438c85cbb3b8ea404fd866
Aleksandra Matiyev <aleksa000777@mail.ru>
2016-02-08 20:22:46 -0500

    adds sphere

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -14,6 +14,7 @@ function today(){
   $('#day').on('click', function(e){
     e.preventDefault();
     $('#description').hide();
+    $('#container').empty();
     $('#today').empty();
     var $data = $('#today');
     $data.show();
@@ -31,20 +32,23 @@ function today(){
 }
 
 
+// var img = '';
 function planet(){
   $('.planet-list').on('click', 'button', function(e){
     e.preventDefault();
     $('#description').show();
     $('#today').empty();
+    $('#container').empty();
     var planet = this.id;
     $.ajax({
       url: '/api/planets/'+planet,
       method: 'get',
       success: function(data){
+        // img = data.Planet.img
         console.log(data.Planet.title);
       $('#title').text(data.Planet.title);
       $('#info').text(data.Planet.info);
-      $('#img img').attr('src', data.Planet.img);
+      // $('#img img').attr('src', data.Planet.img);
       $('#stats').text(data.Planet.stats);
       $('#temp').text(data.Planet.temp);
       $('#size').text(data.Planet.size);
@@ -53,6 +57,11 @@ function planet(){
       $('#moon').text(data.Planet.moon);
       $('#type').text(data.Planet.type);
       $('#distance').text(data.Planet.distance);
+
+
+      createsphere(data.Planet.title);
+      style="background: url('./images/image.jpg')"
+      $('canvas').css("background-image: url('../img/space.jpg')")
       }
     })
   })
