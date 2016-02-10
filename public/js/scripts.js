c4fb1a966c7297599a6297e40ac125678b8a0555
Aleksandra Matiyev <aleksa000777@mail.ru>
2016-02-10 16:21:19 -0500

    adds planets, style

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -8,6 +8,8 @@ function init(){
   today();
   planet();
   main();
+  iframe();
+  starsun();
 
 }
 
@@ -17,22 +19,27 @@ function loadlogo(){
 
 }
 
+
+
 function main(){
   $('#main').on('click', function(e){
     e.preventDefault();
     $('.solar-system').show();
-    $('#description').hide();
+    $('#title,#info,#stats,#temp,#size,#earthday,#earthyear,#moon,#type,#distance').empty();
     $('#container').empty();
     $('#today').empty();
   })
 }
-
+function iframe(){
+    $('iframe').load(function(){$(this).height($(this).contents().outerHeight());$(this).width($(this).contents().outerWidth());});
+}
 
 function today(){
   $('#day').on('click', function(e){
     e.preventDefault();
     $('.solar-system').hide();
-    $('#description').hide();
+    // $('#description').hide();
+    $('#title,#info,#stats,#temp,#size,#earthday,#earthyear,#moon,#type,#distance').empty();
     $('#container').empty();
     $('#today').empty();
     var $data = $('#today');
@@ -43,20 +50,29 @@ function today(){
         success: function(data){
           var $el = $('#today');
           $el.append( $('<h2>').text(data.title) )
-          $el.append( $('<iframe>').attr('src', data.url) )
+          $el.append( $('<iframe>').attr('src', data.hdurl).attr('id','img-day').attr('scrolling', "no" ) );
           $el.append( $('<p>').text(data.explanation) )
         }
     })
   })
 }
 
+function starsun(){
+  $('.sun').on('click','img', function(e){
+    e.preventDefault();
+    $('.solar-system').hide();
+    $('#today').empty();
+    $('#container').empty();
+    var starSun = this.id;
+    console.log(starSun,'sunnnnn');
+    createsphere(starSun);
+  })
+}
 
-// var img = '';
 function planet(){
   $('.planet-list').on('click', 'img', function(e){
     e.preventDefault();
     $('.solar-system').hide();
-    $('#description').show();
     $('#today').empty();
     $('#container').empty();
     var planet = this.id;
@@ -64,24 +80,20 @@ function planet(){
       url: '/api/planets/'+planet,
       method: 'get',
       success: function(data){
-        // img = data.Planet.img
-        console.log(data.Planet.title);
-      $('#title').text(data.Planet.title);
+      $('#title').append( $('<h2>').text(data.Planet.title) );
       $('#info').text(data.Planet.info);
-      // $('#img img').attr('src', data.Planet.img);
-      $('#stats').text(data.Planet.stats);
-      $('#temp').text(data.Planet.temp);
-      $('#size').text(data.Planet.size);
-      $('#earthday').text(data.Planet.earthday);
-      $('#earthyear').text(data.Planet.earthyear);
-      $('#moon').text(data.Planet.moon);
-      $('#type').text(data.Planet.type);
-      $('#distance').text(data.Planet.distance);
+      $('#stats').append($('<h4>').text('Facts:')).append($('<p>').text(data.Planet.stats));
+      $('#temp').append( $('<img>').attr('src', '../img/icons/therm2.png') ).append( $('<p>').text(data.Planet.temp));
+      $('#size').append( $('<img>').attr('src', '../img/icons/size.png') ).append( $('<p>').text(data.Planet.size));
+      $('#earthday').append( $('<h4>').text('Earth times equals 1 day-night:')).append( $('<p>').text(data.Planet.earthday));
+      $('#earthyear').append( $('<h4>').text('Earth times equals 1 year:')).append( $('<p>').text(data.Planet.earthyear));
+      $('#moon').append($('<h4>').text('Moons:')).append($('<p>').text(data.Planet.moon));
+      $('#type').append($('<h4>').text('Type:')).append($('<p>').text(data.Planet.type));
+      $('#distance').append( $('<img>').attr('src', '../img/icons/distance.png') ).append( $('<p>').text(data.Planet.distance) );
+
 
 
       createsphere(data.Planet.title);
-      style="background: url('./images/image.jpg')"
-      $('canvas').css("background-image: url('../img/space.jpg')")
       }
     })
   })
